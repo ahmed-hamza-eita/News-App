9cd1a8c8aa12f969e200f7317e92320be7165fad
aheit <aeita4@yahoo.com>
2024-12-11 02:57:55 +0200

    news service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/ui/home_page.dart';
 
 void main() {
+  getNews();
   runApp(const NewsApp());
 }
+
+final dio = Dio();
+void getNews() async {
+  final response = await dio.get(
+      "https://newsapi.org/v2/everything?q=keyword&apiKey=5619cce537c7439cb56deb903366b2a8");
+
+  print(response);
+}
+
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
@@ -15,4 +26,3 @@ class NewsApp extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/services/news_service.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import '../models/article_model.dart';
+
+class NewsService {
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  Future<List<ArticleModel>> getGeneralNews() async {
+    try {
+      var response = await dio.get(
+          "https://newsapi.org/v2/top-headlines?apikey=5619cce537c7439cb56deb903366b2a8&country=us&category=general");
+
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articleList = articles.map((article) {
+        return ArticleModel(
+          title: article['title'],
+          subTitle: article['description'],
+          image: article['urlToImage'],
+        );
+      }).toList();
+
+      return articleList;
+    } catch (e) {
+      return [];
+    }
+  }
+}
