04d84b48c539272dde105e8cc00a2f8c6f4ddc86
aheit <aeita4@yahoo.com>
2024-12-12 00:37:10 +0200

    refactor future builder widget

--- lib/widgets/news_sliver_list_builder.dart
+++ lib/widgets/news_sliver_list_builder.dart
@@ -1,19 +1,33 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 import 'package:news_app/widgets/progress_loading.dart';
 
 import '../services/news_service.dart';
 import 'news_sliver_list.dart';
 
-class NewsSliverListBuilder extends StatelessWidget {
+class NewsSliverListBuilder extends StatefulWidget {
   const NewsSliverListBuilder({
     super.key,
   });
 
   @override
+  State<NewsSliverListBuilder> createState() => _NewsSliverListBuilderState();
+}
+
+class _NewsSliverListBuilderState extends State<NewsSliverListBuilder> {
+  var future;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    future = NewsService((Dio())).getGeneralNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewsService((Dio())).getGeneralNews(),
+    return FutureBuilder<List<ArticleModel>>(
+        future: future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return NewsSliverList(articles: snapshot.data!);
